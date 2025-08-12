4b7b8ea2833b95d45647e522410065fddf4b6bea
adminmohammed <105809983+adminmohammed@users.noreply.github.com>
2025-08-12 03:21:49 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version "cerulean"
+game "gta5"
+author "Al-Dolime"
+version "1.0.22"
+
+dependencies {
+	'vrp',
+	'vrp_mysql'
+}
+
+client_scripts{ 
+	"lib/Tunnel.lua",
+	"lib/Proxy.lua"	
+}
+
+server_scripts{ 
+  "sv_Config.lua",
+  "@vrp/lib/utils.lua",
+  "File/server.lua"
+}
+
+
+client_script '@vAC/21.lua'
